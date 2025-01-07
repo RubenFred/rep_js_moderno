e7fa5d4630d3a119b87076d418f12273e305209a
fred <fred@gmail.com>
2025-01-06 23:44:11 -0800

    02 variables en javascript

---
+++ 01_fundamentos/02-Variables/js/01-app.js
@@ -0,0 +1,19 @@
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+producto = "Monitor de 19 pulg";
+console.log(producto);
+
+producto = 20
+console.log(producto);
+
+var disponible;
+disponible = true;
+disponible = false;
+
+var categoria = "Computadoras";
+var marca = "marca famosa";
+var calificacion = 5;
+
+var nombreProducto;
+
---
+++ 01_fundamentos/02-Variables/js/02-app.js
@@ -0,0 +1,10 @@
+let producto = 'TABLET';
+
+producto = 'Monitor';
+
+producto = 20;
+console.log(producto);
+
+let precio;
+precio = 300;
+console.log(precio)
