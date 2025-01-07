1aef9fb39e969440e607bc2d41368b089231d5ab
fred <fred@gmail.com>
2025-01-06 22:37:37 -0800

    007  comentarios

--- 01_fundamentos/01-Introduccion/js/app.js
+++ 01_fundamentos/01-Introduccion/js/app.js
@@ -1,3 +1,8 @@
+// pregunta al user su nombre
 const nombre = prompt('Cual es tu nombre?')
 
+// toma el nombre y lo muestra en pantalla
 document.querySelector('.contenido').innerHTML= `${nombre} esta aprendiendo JS moderno`;
+
+// asigna el valor hacia la variable producto
+const producto = "Monitor 24 pulgadas"
